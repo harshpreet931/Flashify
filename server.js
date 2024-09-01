75628a7e6eabba135c685af0f380dff3b858df6d
harshpreet931 <harshpreet0402@gmail.com>
2024-09-01 11:59:43 +0530

    Minor Routing Changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -11,11 +11,11 @@ app.get('/', (req, res) => {
 
 app.get('/createDeck', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'createDeck.html'));
-})
+});
 
 app.get('/quiz', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'quiz.html'));
-})
+});
 
 app.listen(port, () => {
     console.log(`Running at: ${port}`);
