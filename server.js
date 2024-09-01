be72adafb993959a8f3f9c8b89870c22f60726c9
harshpreet931 <harshpreet0402@gmail.com>
2024-09-01 12:20:18 +0530

    Minor Updates

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
