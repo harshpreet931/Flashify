c90172036af1166d0bd6805f001f2a3abf8985fb
harshpreet931 <harshpreet0402@gmail.com>
2024-09-02 07:30:18 +0530

    Starting to change the functionality to show and put multiple decks, only started to update the quiz part

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
+    let deckGrid = document.getElementById('deckGrid');
     let quizContainer = document.getElementById('quizContainer');
     let result = document.getElementById('result');
-    let decks = JSON.parse(localStorage.getItem('decks')) || [];
+    let allDecks = JSON.parse(localStorage.getItem('allDecks')) || {};
+    let currentDeck = [];
+    
+    // ----------------- for Styling and Progress -----------------
+    let progress = document.getElementById('progress');
+    let divBtn = document.getElementsByClassName('btns-styled');
+    let buttons = document.getElementsByClassName('btn-added');
     let correct = 0, wrong = 0;
 
     if(decks.length === 0) {
@@ -54,11 +61,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     quizNext();
-
-
-    let progress = document.getElementById('progress');
-    let divBtn = document.getElementsByClassName('btns-styled');
-    let buttons = document.getElementsByClassName('btn-added');
     quizContainer.className = 'quiz-styled';
     divBtn.className = 'btns-styled';
     buttons.className = 'btn-added';
