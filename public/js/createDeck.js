b44fdbdd5e0051e13692c0637378c4a2b23b0c58
harshpreet931 <harshpreet0402@gmail.com>
2024-09-02 08:18:23 +0530

    Update createDeck.js to support multiple decks and save functionality

--- public/js/createDeck.js
+++ public/js/createDeck.js
@@ -1,21 +1,57 @@
+let currentDeck = [];
+let allDecks = JSON.parse(localStorage.getItem('allDecks')) || {};
+
 document.getElementById('deckForm').addEventListener('submit', function(e) {
     e.preventDefault();
     let deckInput = document.getElementById('deckInput').value;
     
-    let decks = JSON.parse(localStorage.getItem('decks')) || [];
-    decks.push(deckInput);
-    localStorage.setItem('decks', JSON.stringify(decks));
+    currentDeck.push(deckInput);
     
-    alert('Deck saved!');
+    updateDeckShow();
+    clearForm();
+});
+
+function clearForm() {
     document.getElementById('deckInput').value = '';
-});
+}
 
-// showing the currect deck in the div deckShow
+function updateDeckShow() {
+    let deckList = "<ul>";
+    currentDeck.forEach(function(deck) {
+        deckList += `<li>${deck}</li>`;
+    });
+    deckList += "</ul>";
+    document.getElementById('deckShow').innerHTML = deckList;
+}
 
-let decks = JSON.parse(localStorage.getItem('decks')) || [];
-let deckList = "<ul>";
-decks.forEach(function(deck) {
-    deckList += `<li>${deck}</li>`;
-});
-deckList += "</ul>";
-document.getElementById('deckShow').innerHTML = deckList;
+document.getElementById('saveDeck').addEventListener('click', function() {
+    let deckName = document.getElementById('deckName').value;
+    if(deckName && currentDeck.length > 0) {
+        allDecks[deckName] = currentDeck.slice();
+        localStorage.setItem('allDecks', JSON.stringify(allDecks));
+        alert('Deck saved!');
+        currentDeck = [];
+        updateDeckShow();
+        updateAllDecks();
+    }
+    else {
+        alert('Please enter a deck name and at least one card!');
+    }
+}) 
+
+function updateAllDecks() {
+    let decksList = "<ul>";
+    for(let deckName in allDecks) {
+        decksList += `<li onclick="editDeck('${deckName}')">${deckName} (${allDecks[deckName].length} cards)</li>`;
+    }
+    decksList += "</ul>";
+    document.getElementById('allDecks').innerHTML = decksList;
+}
+
+function editDeck(deckName) {
+    currentDeck = allDecks[deckName].slice();
+    document.getElementById('deckName').value = deckName;
+    updateDeckShow();
+}
+
+updateAllDecks();
