9f9af192aa401a4abc56bbe03d8279875c265877
harshpreet931 <harshpreet0402@gmail.com>
2024-09-20 12:49:30 +0530

    Refactor server.js to improve code organization and readability

--- server.js
+++ server.js
@@ -1,26 +1,44 @@
+// Import the Express framework
 const express = require('express');
+
+// Import the path module to work with file and directory paths
 const path = require('path');
+
+// Set the port for the server, using an environment variable if available or defaulting to 3000
 const port = process.env.PORT || 3000;
+
+// Create an instance of an Express app
 const app = express();
 
+// Serve static files from the 'public' directory (e.g., CSS, JS, images)
 app.use(express.static(path.join(__dirname, './public')));
 
-app.get('/', (req, res) => {
+// Route for the root URL ('/'), sending 'index.html' as the response
+app.get('/', (req, res) => 
+{
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/createDeck', (req, res) => {
+// Route for '/createDeck', serving 'createDeck.html' to allow deck creation
+app.get('/createDeck', (req, res) => 
+{
     res.sendFile(path.join(__dirname, 'public', 'createDeck.html'));
 });
 
-app.get('/quiz', (req, res) => {
+// Route for '/quiz', serving 'quiz.html' to allow users to take quizzes
+app.get('/quiz', (req, res) => 
+{
     res.sendFile(path.join(__dirname, 'public', 'quiz.html'));
 });
 
-app.get('/previousScores', (req, res) => {
+// Route for '/previousScores', serving 'previousScores.html' to display past quiz scores
+app.get('/previousScores', (req, res) => 
+{
     res.sendFile(path.join(__dirname, 'public', 'previousScores.html'));
-})
+});
 
-app.listen(port, () => {
+// Start the server and listen on the specified port
+app.listen(port, () => 
+{
     console.log(`Running at: ${port}`);
 });
